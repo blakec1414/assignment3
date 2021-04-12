e7a6659e7b7f3a2d0247e3460662ab40d49d4a71
Blake Cullum <blake.cullum@uleth.ca>
2021-04-12 17:37:13 -0600

    start

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,46 +1,44 @@
-console.log("hello");
+var vidcapture, ctracker, drawcanvas;
+
+function setup(){
+  
+
+  var cnv= createCanvas(windowWidth, windowHeight/2)
+  cnv.parent("p5canvas");
+
+
+vidcapture = createCapture(VIDEO);
+vidcapture.size(vidcapture.width*3, vidcapture.height*4.2)
+
+ctracker = new clm.tracker();
+ctracker.init();
+ctracker.start(vidcapture.elt);
+drawcanvas = document.getElementById('defaultCanvas0');
+
+vidcapture.hide()
+
+
+  var cnv= createCanvas(windowWidth, windowHeight/2)
+  cnv.parent("p5canvas");
+}
+
+function draw(){
+  
+
+image(vidcapture, 0, 0)
+
+var positions = ctracker.getCurrentPosition();
+
+if (positions){
+	//ctracker.draw(drawcanvas);
+	
+}
+}
+
+  
 
 
 
-var $one = $("#j1");
-var $two = $("#a1");
-var $three = $("#j2");
-var $four = $("#a2");
-var $five = $("#j3");
-var $six = $("#a3");
 
-$one.on("click", function() {
-	$(this).hide();
-	$two.show();
-	$("body").css("background-color", "#7209B7");
-});
 
-$two.on("click", function() {
-	$(this).hide();
-	$three.show();
-	$("body").css("background-color", "#3A0CA3");
-});
 
-$three.on("click", function() {
-	$(this).hide();
-	$four.show();
-	$("body").css("background-color", "#4361EE");
-});
-
-$four.on("click", function() {
-	$(this).hide();
-	$five.show();
-	$("body").css("background-color", "#F05365");
-});
-
-$five.on("click", function() {
-	$(this).hide();
-	$six.show();
-	$("body").css("background-color", "#FABC2A");
-});
-
-$six.on("click", function() {
-	$(this).hide();
-	$one.show();
-	$("body").css("background-color", "#F72585");
-});
